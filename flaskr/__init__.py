978ac68f43bc32fa1fe4c73e915a7b391f441df1
Prashant-SK <psk.dev.ca@gmail.com>
2020-04-10 17:40:08 -0700

    added Flask-CORS decorators

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask, jsonify
+from models import setup_db, Plant
+from flask_cors import CORS
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+    # CORS(app, resources={r"*/api/*": {"origins": "*"}})
 
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+   
     @app.route('/')
+    # @cross_origin
     def hello():
         return jsonify({'message': 'HELLO WORLD'})
     
